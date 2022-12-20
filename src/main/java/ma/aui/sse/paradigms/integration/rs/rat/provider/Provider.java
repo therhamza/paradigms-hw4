f13f76bd35a5a69b6f16ea7c5d0dfba58ecde7fb
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-20 20:20:29 +0100

    Update Provider.java

--- src/main/java/ma/aui/sse/paradigms/integration/rs/rat/provider/Provider.java
+++ src/main/java/ma/aui/sse/paradigms/integration/rs/rat/provider/Provider.java
@@ -1,15 +1,22 @@
+// Declare the package for the class
 package ma.aui.sse.paradigms.integration.rs.rat.provider;
+
+// Import the IOException class
 import java.io.IOException;
 
+// Import the SpringApplication and SpringBootApplication classes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Annotation to enable Spring Boot auto-configuration and component scanning
 @SpringBootApplication
 public class Provider {
 
-    public static void main(String[] args) {
-        System.setProperty("java.awt.headless", "false");
-        SpringApplication.run(Provider.class, args);
-    }
-    
+  // The main method is the entry point of the application
+  public static void main(String[] args) {
+    // Set the java.awt.headless property to false
+    System.setProperty("java.awt.headless", "false");
+    // Run the SpringApplication, passing in the Provider class and the command line arguments
+    SpringApplication.run(Provider.class, args);
+  }
 }
