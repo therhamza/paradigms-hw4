d90452b931294b7e712fd3ca983fba31483787f6
Hamza Rehioui <59562086+therhamza@users.noreply.github.com>
2022-12-20 20:15:37 +0100

    Update index.js

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,36 +1,48 @@
+// Import the RxHR module and use it to get a buffer for the specified URL
 const RxHRBuffer = rhr.RxHR.getBuffer('http://localhost:8080/rat/screenshot');
 
+// Get a reference to the 'screenshotBtn' and 'img' elements in the HTML document
 const button = document.getElementById('screenshotBtn');
 const image = document.getElementById('img');
+
+// Declare a variable to store the interval ID
 var intervalId = null;
 
-function onClick () {
-    
-    let n = document.getElementsByTagName('input')[0].value;
+// Set the onclick event for the button to call the onClick function
+button.onclick = onClick;
 
-    if (n <= 0) {
-        alert("Invalid input! Please input a positive number");
-        return;
-    }
+// Define the onClick function, which is called when the button is clicked
+function onClick() {
+  
+  // Get the value of the first input element on the page
+  let n = document.getElementsByTagName('input')[0].value;
 
-    if (intervalId) {
-        clearInterval(intervalId);
-        intervalId = null;
-        button.textContent = "Start";
-        return;
-    }
+  // If the value is less than or equal to 0, display an alert and return
+  if (n <= 0) {
+    alert("Invalid input! Please input a positive number");
+    return;
+  }
 
+  // If the interval ID is set, clear the interval and reset the button text
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    button.textContent = "Start";
+    return;
+  }
+
+  // Subscribe to the RxHRBuffer and update the image src with the data from the API
+  RxHRBuffer.subscribe(data => {
+    image.src = 'data:image/jpeg;base64,' + data.body;
+  });
+
+  // Set an interval to call the RxHRBuffer subscription every n seconds
+  intervalId = setInterval(() => {
     RxHRBuffer.subscribe(data => {
-        image.src ='data:image/jpeg;base64,' + data.body;
+      image.src = 'data:image/jpeg;base64,' + data.body;
     });
+  }, 1000 * n);
 
-    intervalId = setInterval(() => {
-        RxHRBuffer.subscribe(data => {
-            image.src = 'data:image/jpeg;base64,' + data.body;
-        });
-    }, 1000 * n);
-
-    button.textContent = "Stop";
-
-
+  // Update the button text
+  button.textContent = "Stop";
 }
